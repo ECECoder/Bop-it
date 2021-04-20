6329fa1bcf4b11bb7deb725dc234c0ef84b2db30
tys29 <70037495+tys29@users.noreply.github.com>
2021-04-19 22:22:29 -0400

    Add files via upload

--- test.ino
+++ test.ino
@@ -1,16 +1,16 @@
 int score;
 int nextinput;
-unsigned long responsetime = 300;
+unsigned long responsetime = 350;
 bool fail = 1;
 unsigned long timedivider;
 unsigned long prevMillis = 0;
 unsigned long currenttime;
 bool kink;
-int base = 2;
+unsigned long base = 2;
 const int startbutton = 2; 
 const int input0 = 3;
 const int input1 = 0;
-const int input2 = 1;
+const int input2 = 2;
 int read0, read1, read2;
 const int red = 0;
 const int green = 1;
@@ -24,7 +24,7 @@ const int RAPin = 5;
 const int RBPin = 6;
 const int RCPin = 7;
 const int RDPin = 8;
-
+int potCal, potVal;
 
 void setup() {
   pinMode(input1, INPUT);
@@ -48,19 +48,29 @@ void setup() {
 void loop() {
   while(digitalRead(startbutton)==LOW){
     digitalWrite(red, HIGH);
+    digitalWrite(green, HIGH);
+    score = 0;
+    to7seg(score);
   }
 
   digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
   fail = 0;
   prevMillis= millis();
   while(fail==0){
+      
       prevMillis= millis();
-      nextinput = random(0,3);
+      nextinput = 0;
+      potCal = analogRead(input2);
+      read0=0;
+      read1=0;
+      read2=0;
     switch(nextinput){
       case 0: // shake
         kink = 1;
         while(kink==1){
 
+          
          tone(buzzPin,250);
          delay(30);
          noTone(buzzPin);
@@ -68,11 +78,26 @@ void loop() {
 
 
           int shake=analogRead(input0);
-          if(shake<1022) {
+          if(shake>700) { // CHANGE WHEN IN ENCLOSURE
             read0=1;
           } else{
             read0=0;
           }
+
+          
+          lightVal = analogRead(input1);
+          if(lightVal<lightCal-50){
+            read1=1;
+          } else {
+            read1=0;
+          }
+
+         // potVal = analogRead(input2);
+          //if(potVal < potCal - 50 || potVal > potCal + 50) {
+            //read2=1;  
+          //}else{
+            //read2=0;
+         // }
 
           
           currenttime = millis();
@@ -82,21 +107,18 @@ void loop() {
             score++;
             to7seg(score);
             if(score<20){
-              timedivider = timedivider+.000001;
+              responsetime=responsetime-5;
             }
-            responsetime = responsetime/timedivider;
             kink = 0;
             digitalWrite(1, HIGH);
             delay(250);
             digitalWrite(1, LOW);
-          } else if(read0==0 && currenttime - prevMillis > responsetime){
+          } else if((read0==0 && currenttime - prevMillis > responsetime) || read1!=0 || read2!=0){
             digitalWrite(0, HIGH);
-            digitalWrite(1, HIGH);
             fail = 1;
             kink = 0;
             delay(250);
             digitalWrite(0, LOW);
-            digitalWrite(1, LOW);
           }
         }
         break;
@@ -104,10 +126,18 @@ void loop() {
         kink = 1;
         while(kink==1){
 
+        
          tone(buzzPin,1000);
          delay(30);
          noTone(buzzPin);
 
+
+          int shake=analogRead(input0);
+          if(shake<200) { // CHANGE WHEN IN ENCLOSURE
+            read0=1;
+          } else{
+            read0=0;
+          }
 
           
           lightVal = analogRead(0);
@@ -117,6 +147,12 @@ void loop() {
             read1=0;
           }
 
+          potVal = analogRead(input2);
+          if(potVal < potCal - 50 || potVal > potCal + 50) {
+            read2=1;  
+          }else{
+            read2=0;
+          }
 
 
           currenttime = millis();
@@ -125,21 +161,18 @@ void loop() {
             score++;
             to7seg(score);
             if(score<20){
-              timedivider=timedivider+.000001;
+               responsetime=responsetime-5;
             }
-            responsetime=responsetime/timedivider;
             kink = 0;
             digitalWrite(1, HIGH);
             delay(250);
             digitalWrite(1, LOW);
-          } else if (read1==0 && currenttime - prevMillis > responsetime){
+          } else if ((read1==0 && currenttime - prevMillis > responsetime) || read0!=0 || read2!=0){
             digitalWrite(0, HIGH);
-            digitalWrite(1, HIGH);
             fail = 1;
             kink = 0;
             delay(250);
             digitalWrite(0, LOW);
-            digitalWrite(1, LOW);
           }
         }
         break;
@@ -147,14 +180,30 @@ void loop() {
         kink = 1;
         while(kink==1){
 
+          
           tone(buzzPin,2000);
           delay(30);
           noTone(buzzPin);
 
 
-          int sensorValue = analogRead(input2);
-          int voltage = sensorValue * (5.0/1023.0);
-          if(voltage<4) {
+          int shake=analogRead(input0);
+          if(shake<200) { // CHANGE WHEN IN ENCLOSURE
+            read0=1;
+          } else{
+            read0=0;
+          }
+
+          
+          lightVal = analogRead(0);
+          if(lightVal<lightCal-50){
+            read1=1;
+          } else {
+            read1=0;
+          }
+
+          
+          potVal = analogRead(input2);
+          if(potVal < potCal - 50 || potVal > potCal + 50) {
             read2=1;  
           }else{
             read2=0;
@@ -168,21 +217,18 @@ void loop() {
             score++;
             to7seg(score);
             if(score<20){
-              timedivider=timedivider+.000001;
+              responsetime=responsetime-5;
             }
-            responsetime=responsetime/timedivider;
             kink = 0;
             digitalWrite(1, HIGH);
             delay(250);
             digitalWrite(1, LOW);
-          } else if (read2==0 && currenttime - prevMillis > responsetime){
+          } else if ((read2==0 && currenttime - prevMillis > responsetime) || read0!=0 || read1!=0){
             digitalWrite(0, HIGH);
-            digitalWrite(1, HIGH);
             fail = 1;
             kink = 0;
             delay(250);
             digitalWrite(0, LOW);
-            digitalWrite(1, LOW);
           }
         }
         break;
